12c6cf4ef1713350be368115fe5c4fd3c3ff361b
John Eipe <johne@INLM-JohnE.local>
2020-07-23 11:02:30 +0530

    Removed stale files

--- apiserver/src/test/java/com/apiserver/NativeCodeResourceIT.java
+++
@@ -1,9 +0,0 @@
-package com.apiserver;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeCodeResourceIT extends CodeResourceTest {
-
-    // Execute the same tests but in native mode.
-}
